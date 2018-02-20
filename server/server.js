fb6bdde421a356f9d1aac6c22d8ecf5e0d80a29c
yousif al-bayaty <yalbayaty@zomaron.com>
2018-02-20 16:10:54 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 
 
 const {mongoose} = require('./db/mongoose');
@@ -28,6 +28,14 @@ app.post('/todos', (req, res) =>{
     });
 })
 
+
+app.get('/todos', (req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e) =>{
+        res.status(400).send(e);
+    })
+});
 
 //GET todos/2131289d
 
