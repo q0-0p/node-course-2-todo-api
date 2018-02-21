ae8282892a7df57c3a3dfd6865589b9fee5a8cf8
yousif al-bayaty <yalbayaty@zomaron.com>
2018-02-21 16:23:33 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/users');
+const {ObjectID} = require('mongodb');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/users');
 
 
 
@@ -16,35 +16,64 @@ app.use(bodyParser.json());
 
 //CRUD
 
-app.post('/todos', (req, res) =>{
+app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    }, (e) =>{
+    }, (e) => {
         res.status(400).send(e);
     });
 })
 
 
-app.get('/todos', (req,res)=>{
-    Todo.find().then((todos)=>{
-        res.send({todos});
-    }, (e) =>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
         res.status(400).send(e);
     })
 });
 
 //GET todos/2131289d
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // var id = '5a8c906b33f7efc15aeab16a';
+
+
+    //validate ID is correct using isValid
+    //respond with 404 if its not found - send back empty body (send())
+    if (!ObjectID.isValid(id)) {
+       return res.status(404).send();
+    }
+//findById using the id param
+    //success
+    //if todo - send it back
+    //if no todo - send back 404 with empty body
+    
+
+    Todo.findById(id).then((todo) =>{
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+
+    //error
+    //400 - send() empty body back
+});
 
 
 
 
-
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
